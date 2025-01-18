d149ca96b9c5490a4be26afa9284d392022d46b4
Nikhil Dasar <kakashihatakesh6@gmail.com>
2025-01-18 17:29:31 +0530

    updated next-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
-
-export default nextConfig;
-
-module.exports = {
-  async headers() {
+    reactStrictMode: true,
+    async headers() {
       return [
-          {
-              source: '/api/:path*',
-              headers: [
-                  {
-                      key: 'Strict-Transport-Security',
-                      value: 'max-age=31536000; includeSubDomains'
-                  },
-                  {
-                      key: 'X-Content-Type-Options',
-                      value: 'nosniff'
-                  },
-                  {
-                      key: 'X-Frame-Options',
-                      value: 'DENY'
-                  },
-                  {
-                      key: 'X-XSS-Protection',
-                      value: '1; mode=block'
-                  }
-              ]
-          }
+        {
+          source: '/api/:path*',
+          headers: [
+            {
+              key: 'Strict-Transport-Security',
+              value: 'max-age=31536000; includeSubDomains'
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff'
+            },
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY'
+            },
+            {
+              key: 'X-XSS-Protection',
+              value: '1; mode=block'
+            }
+          ]
+        }
       ]
-  }
-}
+    }
+  };
+  
+  export default nextConfig;
