c24018240a33037aca6eb8a6c9d076bf9d2743b4
Nikhil <nikhil@gpu.net>
2026-02-23 18:03:34 +0530

    fix: login

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -126,17 +126,6 @@ export const authOptions: AuthOptions = {
       return session as ExtendedSession;
     },
   },
-  cookies: {
-    sessionToken: {
-      name: `next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: process.env.NODE_ENV === "production",
-      },
-    },
-  },
   secret: process.env.NEXTAUTH_SECRET,
 };
 
